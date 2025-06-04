338fc239979523069c196326271055a86a0e73c4
PierreMarza <pierre.marza@gmail.com>
2025-06-04 10:26:00 +0200

    Added imagenet normalization to dinov2small custom model example

--- examples/dinov2small.py
+++ examples/dinov2small.py
@@ -13,6 +13,9 @@ class DINOv2Features(PretrainedModel):
             [
                 transforms.ToTensor(),
                 transforms.Resize((224, 224)),
+                transforms.Normalize(
+                    mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)
+                ),
             ]
         )
         self.name = "dinov2_vits14"
