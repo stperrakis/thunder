e8af10331309e09b60df7d9cc71832ecf553d0e8
PierreMarza <pierre.marza@gmail.com>
2025-07-10 10:53:12 +0200

    Updated setup name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="thundr",
+    name="thunder-bench",
     version="0.1.0",
     description="THUNDER: Tile-level Histopathology image UNDERstanding benchmark",
     long_description=open("README.md").read(),
